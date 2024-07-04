56c995253154df6abe17a6d0154a54bf501aecb8
adiaditya2579 <adiaditya2579@gmail.com>
2024-07-04 18:05:14 +0530

    first commit

---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Location = () => {
+  return (
+    <>
+      <span id="location"></span>
+      <section data-aos="fade-up" className="">
+        <div className="container my-4">
+          <h1 className="inline-block border-l-8 border-primary/50 py-2 pl-2 mb-4 text-xl font-bold sm:text-3xl">
+            Location to visit
+          </h1>
+
+          <div className="rounded-xl ">
+            <iframe
+              src="https://www.google.com/9a7ce4c1e7dccb:0xfef6562cafd06dd3!8m2!3d26.2585371!4d82.0659858!16zL20vMDc0cF9i?entry=ttu"
+              width="100%"
+              height="360"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              style={{ borderRadius: "20px" }}
+            ></iframe>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Location;
